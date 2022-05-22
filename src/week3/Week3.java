176158b4fde0c7765797436f41d9e0149cf6e8a1
siasonchris <siasonc@sheridancollege.ca>
2022-05-22 11:45:58 -0400

    version 1

---
+++ src/week3/Week3.java
@@ -0,0 +1,18 @@
+
+package week3;
+
+/**
+ *
+ * @author chris
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("learning github");
+       
+    }
+
+}
